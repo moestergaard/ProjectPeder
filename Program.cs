5b00786edb3130b683ff12cb82c685c6f5ff8058
Martin Oestergaard <martin.malmkjaer.oestergaard@gmail.com>
2022-12-12 21:53:36 +0100

    Template Updated

--- Program.cs
+++ Program.cs
@@ -1,2 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ProjectPeder
+{
+    class Program
+    {
+        public static Boolean Done = false;
+        public static Int32 TotalWorkingHoursWeekdays = 0;
+        public static Int32 TotalWorkingHoursWeekends = 0;
+
+        public static String[] day = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        public static Int32 dayNumber = 0; 
+
+        // Enter more variables here if needed.
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Call the method
+            GetInformation();
+            PrintData();
+        }
+        
+        /// <summary>
+        /// This method will get information from the user.
+        /// </summary>
+
+        static void GetInformation()
+        {
+            Console.WriteLine("Getting Information");
+
+            while(!Done)
+            {
+                // You may want to change the text here.
+                Console.WriteLine("Enter a string: ");
+                String? str = Console.ReadLine();
+                if(str != null) {
+                    UpdateInformation(str);
+                }
+        
+                // Give the user a text that tells them what information you want.
+                // Maybe use the method UpdateDate() to update the date after each input.
+            }
+        }
+
+        /// <summary>
+        /// This method will update the information.
+        /// </summary>
+        /// <param name="str"></param>
+        /// Take care of each possible input string and update the information accordingly.
+        static void UpdateInformation(String str)
+        {
+            if (str.ToLower().Contains("help"))
+            {
+                Console.WriteLine(
+                    "\n********************************* \n " +
+                    "Enter 'done' to exit the program. \n " +
+                    "Enter 'help' to get help. \n" +
+                    "********************************* \n ");
+            }
+            else if (str.ToLower().Contains("done"))
+            {
+                Done = true;
+            }
+
+            // Take care of inputs of data that is not 'done' or 'help'
+            // For example, if the user enters working hours, you can update variables that keep track of the total working hours.
+            else
+            {
+                Console.WriteLine("You entered: " + str);
+            }
+        }
+
+        static void UpdateDate()
+        {
+            // This method will update the date.
+            // You can use the variables day and dayNumber to keep track of the date.
+        }
+
+        /// <summary>
+        /// Here you can print the information you have gathered, and style as you wish.
+        /// See the example in UpdateInformation() for how to print a string with multiple lines.
+        /// If you want to print a variable, you can use the + operator.
+        /// </summary>
+        static void PrintData() 
+        {
+            Console.WriteLine("\n \nPrinting Data");
+            Console.WriteLine("Total working hours weekdays: " + TotalWorkingHoursWeekdays);
+            Console.WriteLine("Total working hours weekends: " + TotalWorkingHoursWeekends);
+            Console.WriteLine("day: " + day[dayNumber]);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # ProjectPeder
+
+For at køre programmet kan du i terminalen skrive
+    
+    dotnet run
+
+Derefter skriver den i terminalen, og du kan også skrive deri.
+
+Efter du har arbejdet, kan du med fordel bruge Git til at gemme dit arbejde.
+Det gør du ved at skrive følgende i terminalen.
+
+    git commit -am "en besked til dig selv"
+    git push
+
+På den måde bliver alle dine ændringer lagt op i skyen.
+Hvis du får problemer, kan du også nemt gå tilbage til et tidligere commit, hvor det virkede.
+Derudover hvis du får brug for, at jeg ser på noget, kan jeg nemt hente det ned på min egen maskine.
+Hvis du vælger at bruge Git, kan du med fordel hver gang du starter med at arbejde skrive 
+
+    git pull
+
+På den måde trækker du eventuelle ændringer ned fra skyen. Når du bare skriver selv, burde der ingen ændringer være,
+men det kan være fint nok at vænne sig til.
+
+Hvis du vil have det ud i en fil, kan du skrive sådan her i terminalen.
+
+    dotnet run > text.txt
+
+Det har dog den ulempe, at du skal åbne og lukke text.txt filen på ny hver gang for at se ændringer.
+Det skal jeg nok lige prøve at få set på. Du kan navngive filen som du vil, 
+og du kan også bagefter slette noget af filen, hvis du kun vil beholde den del, hvor du printer dine data.
